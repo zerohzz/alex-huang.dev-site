d3fb465745e509f40c28790a7561a8aa9a8898c5
zerohzz <zerohzz@users.noreply.github.com>
2026-07-02 05:24:39 +0000

    deploy: a7eb21db3f4f706c8078da57e6febeabe91d5cf0 zerohzz/alex-huang.dev@a7eb21db3f4f706c8078da57e6febeabe91d5cf0

---
+++ _astro/ask.astro_astro_type_script_index_0_lang.CvCdLPtO.js
@@ -0,0 +1,1 @@
+import{a}from"./askAlex.BuJFlm-s.js";document.addEventListener("astro:page-load",()=>{const t=document.getElementById("aa-ask");t&&a(t)});
--- _astro/ask.astro_astro_type_script_index_0_lang.DKWWjBgd.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./askAlex.9RWdU2Lr.js";document.addEventListener("astro:page-load",()=>{const t=document.getElementById("aa-ask");t&&a(t)});
